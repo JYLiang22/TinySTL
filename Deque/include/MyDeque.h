4e7e0ceec64d6bd51f9548952a2783c03eaf651d
JYLiang22 <liangjyno1@gmail.com>
2024-04-28 11:21:21 +0800

    更新Deque代码

---
+++ Deque/include/MyDeque.h
@@ -0,0 +1,29 @@
+#ifndef _MYDEQUE_H_
+#define _MYDEQUE_H_
+
+template<typename T>
+class MyDeque{
+private:
+    T* elements;   // 一个循环一维数组
+    size_t capacity;   // 数组总容量
+    size_t size;   // 元素数量
+    size_t frontIdx;   // deque前端索引
+    size_t backIdx;   // deque后端索引
+
+public:
+    MyDeque();
+    ~MyDeque();
+    void push_front(const T& val);
+    void push_back(const T& val);
+    void pop_front();
+    void pop_back();
+    T& operator[](int idx);
+    size_t getSize() const;
+    void clear();
+    void printElements() const;
+
+private:
+    void resize();
+};
+
+#endif
